d0d897236393273943d5d33fa8a4503c97d51ac1
Mohammad-Abdelhadi <mohammad.b.abdelhadi@gmail.com>
2023-06-18 10:36:57 +0300

    add the import of bootstrap to app js

--- finance/src/App.js
+++ finance/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
-
+// bootstrap imports
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/js/bootstrap.js';
 function App() {
   return (
     <div className="App">
