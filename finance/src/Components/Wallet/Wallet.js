965040fb805129a3d1d99ce8992f803c5512e4f7
Mohammad-Abdelhadi <mohammad.b.abdelhadi@gmail.com>
2023-06-20 00:12:28 +0300

    add the transictions in the page (add , pay , income )

--- finance/src/Components/Wallet/Wallet.js
+++ finance/src/Components/Wallet/Wallet.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Mobile from '../../Images/Mobile.svg'
 import Arrow from '../../Images/ArrowBack.svg'
-
+import bell from "../../Images/bell.svg"
+import Add from "../../Images/Add.svg"
+import Pay from "../../Images/Pay.svg"
+import Income from "../../Images/income.svg"
 import "./Wallet.css";
+import { Link } from 'react-router-dom';
 
 function Wallet() {
   return (
@@ -11,16 +15,45 @@ function Wallet() {
 <div className='buttons'> 
 <p><img src={Arrow} alt="" /></p>
 <p id='wallet'>Wallet</p>
-<p>o</p>
+<p><img src={bell} alt="" /></p>
 </div>
       {/* First Background */}
       <div className="first-background">
         {/* Second Background */}
         <div className="second-background">
-          <p className="text">Total Balance</p>
-          <p className="text">Some text here</p>
-          <p className="text">Some text here</p>
+          <div className='Content'
+          >
+            <p className="normal-text">Total Balance</p>
+            <p className="text">$2,549.00</p>
+
+                </div>
+        <div className='Transictions'>
+       <div className='internal-content'>
+        <Link to="/expense">     <img src={Add} alt="" /></Link>
+        <Link to="/expense">          <img src={Pay} alt="" />
+ </Link>
+        <Link to="/expense"> 
+        <img src={Income} alt="" />
+        
+        </Link>
+     
+      
+   
+
+ 
         </div>
+        <div className='internal-titles'>
+        <span>Add</span>
+        <span>Pay</span>
+        <span>Income</span>
+      
+
+        </div>
+ 
+
+        </div>
+        </div>
+          
       </div>
     </div>
   );
