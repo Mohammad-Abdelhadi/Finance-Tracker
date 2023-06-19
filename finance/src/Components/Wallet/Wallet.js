50054062f67974df30baec24791d8d4499e56f40
Mohammad-Abdelhadi <mohammad.b.abdelhadi@gmail.com>
2023-06-19 22:41:01 +0300

    add the phone bar of icons , add the backgrounds in wallet page

--- finance/src/Components/Wallet/Wallet.js
+++ finance/src/Components/Wallet/Wallet.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Mobile from '../../Images/Mobile.svg'
+import Arrow from '../../Images/ArrowBack.svg'
 
-const Wallet = () => {
+import "./Wallet.css";
+
+function Wallet() {
   return (
-    <div>Wallet</div>
-  )
+    <div className="wallet-container">
+      <img src={Mobile} alt="" className='phoneBar'/>
+<div className='buttons'> 
+<p><img src={Arrow} alt="" /></p>
+<p id='wallet'>Wallet</p>
+<p>o</p>
+</div>
+      {/* First Background */}
+      <div className="first-background">
+        {/* Second Background */}
+        <div className="second-background">
+          <p className="text">Total Balance</p>
+          <p className="text">Some text here</p>
+          <p className="text">Some text here</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Wallet
+export default Wallet;
