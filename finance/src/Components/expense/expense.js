2a0f2f18ee7992e6390e114c590483734aae7d6c
Ehab-Aqili <ihabaloqaily@gmail.com>
2023-06-19 11:03:04 +0300

    add men image to images file

--- finance/src/Components/expense/expense.js
+++ finance/src/Components/expense/expense.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./expense.css";
+import Mobile from '../../Images/Mobile.svg'
+import ArrowBack from '../../Images/ArrowBack.svg'
+import { Link } from "react-router-dom";
+
+
 
 const Expense = () => {
   return (
     <>
       <main className="container">
         <div className="row">
-          <div className="col-12 p-5"><img alt="#" src="./img/Mobile.svg" /></div>
+          <div className="col-12 "><img alt="#" src={Mobile} id="mobile__icon"/></div>
+          <div className="col-12">
+            <div className=""> <img alt="#" src={ArrowBack} className="" /> </div>
+            <div className=""><Link className="text-decoration-none" to="#">Add Expense or Income</Link></div>
+            <div className=""></div>
+            </div>
         </div>
       </main>
     </>
