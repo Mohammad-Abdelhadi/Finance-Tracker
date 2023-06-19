9675fd8cf63bdf1dbb3dad7378ffbfc69a30bee0
Mohammad-Abdelhadi <mohammad.b.abdelhadi@gmail.com>
2023-06-19 10:33:09 +0300

    add the routes for all the pages

--- finance/src/App.js
+++ finance/src/App.js
@@ -6,13 +6,24 @@ import Splash from "./Components/Splash/splash";
 
 import { Routes,Route } from 'react-router-dom';
 import Home from './Components/Home/Home';
+import Onboarding from "./Components/onboarding/onboarding";
+import Signin from "./Components/sign_in/signin";
+import Signup from "./Components/sign_up/signup";
+import Statistic from "./Components/Statistic/Statistic";
+import Wallet from "./Components/Wallet/Wallet";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Splash />} />
-        {/* <Route path='Signup' element={<Signup/>} /> */}
+        <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/HomePage" element={<Home />} />
         <Route path="/expense" element={<Expense />} />
+        <Route path="/statistic" element={<Statistic/>} />
+        <Route path="/Wallet" element={<Wallet />} />
+        {/* <Route path='Signup' element={<Signup/>} /> */}
       </Routes>
     </div>
   );
