4451c401a89b41ba17701a67486f7659383b87fe
Ehab-Aqili <ihabaloqaily@gmail.com>
2023-06-22 10:17:08 +0300

    add firebase function addDoc and getDocs

--- finance/src/Components/Home/Home.js
+++ finance/src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import topBackground from "./Home.css";
 import "./Home.css";
 import ring from "../../Images/ring.svg";
@@ -8,12 +8,33 @@ import whitearrow from "../../Images/whitearrow.svg";
 import incomeArrow from "../../Images/incomeArrow.svg";
 import expensesArrow from "../../Images/expensesArrow.svg";
 import upwork from "../../Images/upwork.svg";
-
-
-
+// import from firebase to get data
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "./../expense/config/firebase";
 
 const Home = () => {
- 
+  // Function that get the data from FirBase
+  const [categoriesList, setCategoriesList] = useState([]);
+  const expenseCollectionRef = collection(db, "expenses");
+  const getCategoriesList = async () => {
+    const data = await getDocs(expenseCollectionRef);
+    const filterData = data.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    console.log(filterData);
+    setCategoriesList(filterData);
+  };
+  useEffect(() => {
+    getCategoriesList();
+  }, []);
+
+  let sumExpense = 0;
+  categoriesList.forEach((card) => {
+    sumExpense += card.expense;
+  });
+
+  // End code of Function that get the data from FirBase
   return (
     <>
       <div className="home-container">
@@ -33,125 +54,71 @@ const Home = () => {
 
             <div className="name-container">
               <div>
-              <p>Welcome</p>
-              <p>Angela Jone</p>
+                <p>Welcome</p>
+                <p>Angela Jone</p>
               </div>
               <img src={ring} alt="" />
             </div>
-            
           </div>
           <div className="warpper">
-          <div className="profile-info-container">
-           
-            <div className="balance-container">
-           <div className="total-balance">
-            <p>Total Balance</p>
-            <img src={whitearrow} alt="" />
-           </div>
-           <div>
-           <img src={dots} alt="" />
-           </div>
-           </div>
-           <p className="balance">$2985.00</p>
-           <div  className="expense-container">
-              <div className="income">
-              <img src={incomeArrow} alt="" />
-              <p>Income</p>
+            <div className="profile-info-container">
+              <div className="balance-container">
+                <div className="total-balance">
+                  <p>Total Balance</p>
+                  <img src={whitearrow} alt="" />
+                </div>
+                <div>
+                  <img src={dots} alt="" />
+                </div>
               </div>
-              <div className="expenses">
-              <img src={expensesArrow} alt="" />
-              <p>expenses</p>
+              <p className="balance">${sumExpense}</p>
+              <div className="expense-container">
+                <div className="income">
+                  <img src={incomeArrow} alt="" />
+                  <p>Income</p>
+                </div>
+                <div className="expenses">
+                  <img src={expensesArrow} alt="" />
+                  <p>expenses</p>
+                </div>
               </div>
-           </div>
-            <div className="expenses-values">
-              <div className="income-value">
-                <p>$1840.00</p>
-              </div>
-              <div className="expense-value">
-                <p>$284.00</p>
+              <div className="expenses-values">
+                <div className="income-value">
+                  <p>$1840.00</p>
+                </div>
+                <div className="expense-value">
+                  <p>$284.00</p>
+                </div>
               </div>
             </div>
           </div>
-          </div>
         </div>
-          <div className="transiction-container">
-            <div className="Transiction-history">
+        <div className="transiction-container">
+          <div className="Transiction-history">
             <p>Transiction history</p>
             <p>see all</p>
-            </div>
-        <div className="transiction__homepage">
-          <div className="left-side">
-          <div><img src={upwork} alt="" /></div>
-          <div>
-            <p>Upwork</p>
-            <p>Today</p>
           </div>
-          </div>
-          <div>
-            <p>-$800</p>
-          </div>
+          {categoriesList
+            .sort((a, b) => new Date(b.date) - new Date(a.date))
+            .map((value) => {
+              return (
+                <div className="transiction__homepage" key={value.id}>
+                  <div className="left-side">
+                    <div>
+                      <img src={upwork} alt="" />
+                    </div>
+                    <div>
+                      <p>{value.categories}</p>
+                      <p>{value.date}</p>
+                    </div>
+                  </div>
+                  <div>
+                    <p style={{ color: "red" }}>-${value.expense}</p>
+                  </div>
+                </div>
+              );
+            })}
         </div>
-        <div className="transiction__homepage">
-          <div className="left-side">
-          <div><img src={upwork} alt="" /></div>
-          <div>
-            <p>Upwork</p>
-            <p>Today</p>
-          </div>
-          </div>
-          <div>
-            <p>-$800</p>
-          </div>
-        </div>
-        <div className="transiction__homepage">
-          <div className="left-side">
-          <div><img src={upwork} alt="" /></div>
-          <div>
-            <p>Upwork</p>
-            <p>Today</p>
-          </div>
-          </div>
-          <div>
-            <p>-$800</p>
-          </div>
-        </div>
-        <div className="transiction__homepage">
-          <div className="left-side">
-          <div><img src={upwork} alt="" /></div>
-          <div>
-            <p>Upwork</p>
-            <p>Today</p>
-          </div>
-          </div>
-          <div>
-            <p>-$800</p>
-          </div>
-        </div>
-        <div className="transiction__homepage">
-          <div className="left-side">
-          <div><img src={upwork} alt="" /></div>
-          <div>
-            <p>Upwork</p>
-            <p>Today</p>
-          </div>
-          </div>
-          <div>
-            <p>-$800</p>
-          </div>
-        </div>
-        <div className="transiction__homepage">
-          <div className="left-side">
-          <div><img src={upwork} alt="" /></div>
-          <div>
-            <p>Upwork</p>
-            <p>Today</p>
-          </div>
-          </div>
-          <div>
-            <p>-$800</p>
-          </div>
-        </div>
-      </div>
       </div>
     </>
   );
